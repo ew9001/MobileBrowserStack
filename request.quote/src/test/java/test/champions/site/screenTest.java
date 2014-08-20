462a614e19aae55c4fb2c5229193b4e2aa35a6c3
Earl Wilis <earl.willis@publicismodem.com>
2014-08-20 14:00:05 -0400

    changed

--- request.quote/src/test/java/test/champions/site/screenTest.java
+++ request.quote/src/test/java/test/champions/site/screenTest.java
@@ -133,7 +133,7 @@ public class screenTest {
 	        	driver.manage().timeouts().implicitlyWait(80000, TimeUnit.SECONDS);
 		        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		        JavascriptExecutor jse = (JavascriptExecutor)driver;
-		        jse.executeScript("scroll(0, 100)"); //y value '250' can be altered
+		        //jse.executeScript("scroll(0, 100)"); //y value '250' can be altered
 		        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		        if (browser.equals("firefox")||browser.equals("ie9")||browser.equals("chrome")||browser.equals("ie10")||browser.equals("safari")||browser.equals("ie8")) {
 		        driver.findElement(By.id("tile3")).click();
